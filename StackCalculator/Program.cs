5757fb3efc496c5bdbd1f56483ab2937c10229e7
simasbakus <simas.bakus@gmail.com>
2021-12-14 17:48:57 +0200

    initial commit

---
+++ StackCalculator/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StackCalculator
+{
+    class Program
+    {
+        public static Stack<UInt10> MyStack = new();
+        public static bool Run = true;
+
+        static void Main(string[] args)
+        {
+            while (Run)
+            {
+                string input = Console.ReadLine();
+
+                if (input.StartsWith("push", StringComparison.CurrentCultureIgnoreCase))
+                    Push((UInt10)input[5..]);
+                else
+                {
+                    switch (input.Replace(" ", string.Empty).ToLower())
+                    {
+                        case "pop":
+                            Pop();
+                            break;
+
+                        case "add":
+                            Add();
+                            break;
+
+                        case "sub":
+                            Sub();
+                            break;
+
+                        case "quit":
+                            Quit();
+                            break;
+
+                        default:
+                            Console.WriteLine("Such command does not exist!");
+                            break;
+                    }
+                }
+            }
+        }
+
+        static void Push(UInt10 value)
+        {
+            if (MyStack.Count >= 5)
+                Console.WriteLine("Value not inserted. Stack limit reached!");
+            else
+                MyStack.Push(value);
+        }
+
+        static UInt10 Pop()
+        {
+            return MyStack.Count > 0 ? MyStack.Pop() : 0;
+        }
+
+        static void Add()
+        {
+            if (MyStack.Count >= 2)
+            {
+                UInt10 result = Pop() + Pop();
+                Push(result);
+                Console.WriteLine($"Result: {MyStack.Peek()}");
+            }
+            else
+                Console.WriteLine("Not enough values inserted!");
+        }
+
+        static void Sub()
+        {
+            if (MyStack.Count >= 2)
+            {
+                UInt10 result = Pop() - Pop();
+                Push(result);
+                Console.WriteLine($"Result: {MyStack.Peek()}");
+            }
+            else
+                Console.WriteLine("Not enough values inserted!");
+        }
+
+        static void Quit()
+        {
+            Console.WriteLine("Do You really want to quit? y | n");
+
+            if (Console.ReadLine().StartsWith("y", StringComparison.CurrentCultureIgnoreCase))
+                Run = false;
+        }
+    }
+}
---
+++ StackCalculator/UInt10.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackCalculator
+{
+    public struct UInt10
+    {
+        private UInt10(ushort value)
+        {
+            if (value >= (1 << 10))
+                throw new ArgumentOutOfRangeException(nameof(value));
+
+            this.value = value;
+        }
+
+        private readonly ushort value;
+
+        public static implicit operator UInt10(ushort value)
+        {
+            return new UInt10(value);
+        }
+
+        public static explicit operator UInt10(string value)
+        {
+            return new UInt10(Convert.ToUInt16(value));
+        }
+
+        public static explicit operator ushort(UInt10 uint10)
+        {
+            return uint10.value;
+        }
+
+        public static UInt10 operator +(UInt10 a, UInt10 b)
+        {
+            var result = a.value + b.value;
+
+            if (result >= (1 << 10))
+                return (UInt10)(result - (1 << 10));
+
+            return (UInt10)result;
+        }
+        
+        public static UInt10 operator -(UInt10 a, UInt10 b)
+        {
+            var result = a.value - b.value;
+
+            if (result < 0)
+                return (UInt10)((1 << 10) + result);
+
+            return (UInt10)(a.value - b.value);
+        }
+
+        public override string ToString()
+        {
+            return value.ToString();
+        }
+    }
+}
